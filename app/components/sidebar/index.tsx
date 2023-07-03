61fca68b857b5538c1f85d6540b9cca1b46d1baa
mhmddev <mabrahimibagha@gmail.com>
2023-07-03 22:32:42 +0330

    feat: add profile story

---
+++ app/components/sidebar/index.tsx
@@ -0,0 +1,12 @@
+import ProfileStory from "@/app/components/sidebar/profile-story";
+
+const Sidebar = () => {
+    return (
+        <div className="py-4 px-3">
+            {/* profile story */}
+            <ProfileStory/>
+        </div>
+    )
+}
+
+export default Sidebar
