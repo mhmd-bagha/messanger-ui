bac7190b597875937cd412ada98962cac21b7a76
mhmddev <mabrahimibagha@gmail.com>
2023-07-04 08:49:27 +0330

    refactor: change button to p

--- app/components/sidebar/profile-story.tsx
+++ app/components/sidebar/profile-story.tsx
@@ -6,9 +6,9 @@ const ProfileStory = () => {
         <button className="relative rounded-full border-2 border-pink-red">
             <Image src="/images/user/profile/profile.jpg" alt="user" width={50} height={50}
                    className="rounded-full p-0.5"/>
-            <button
+            <p
                 className="bg-blue-picton-rgb text-white rounded-full border border-dark-jungle absolute -bottom-1 right-0">
-                <BsPlus/></button>
+                <BsPlus/></p>
         </button>
     )
 }
