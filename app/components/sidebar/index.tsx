f9e4c926fcd302cda9c9b466ebfb6b60290d0dd5
mhmddev <mabrahimibagha@gmail.com>
2023-07-04 19:29:03 +0330

    feat: add change light/dark mode

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -1,5 +1,6 @@
 import ProfileStory from "@/app/components/sidebar/profile-story";
 import Folders from "@/app/components/sidebar/folders";
+import ChangeMode from "@/app/components/sidebar/changeMode";
 
 const Sidebar = () => {
     return (
@@ -8,6 +9,8 @@ const Sidebar = () => {
             <ProfileStory/>
             {/* folders */}
             <Folders/>
+            {/* dark and light mode */}
+            <ChangeMode/>
         </div>
     )
 }
