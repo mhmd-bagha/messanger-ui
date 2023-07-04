dba4f438acc4ced47564f600db68f199951f639b
mhmddev <mabrahimibagha@gmail.com>
2023-07-04 14:58:59 +0330

    feat: add folders

---
+++ app/components/sidebar/folders.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import {FoldersChat} from "@/app/types/sidebar";
+import {BiSolidMessage} from "react-icons/bi";
+import {BsFillBookmarkDashFill, BsFillTelephoneFill, BsFolderFill} from "react-icons/bs";
+import {useState} from "react";
+import {FaUserGroup} from "react-icons/fa6";
+import {RiSettingsFill} from "react-icons/ri";
+
+const Folders = () => {
+    const [idButton, setIdButton] = useState<number>(0)
+
+    const folders: FoldersChat[] = [
+        {
+            title: 'chats',
+            icon: BiSolidMessage
+        },
+        {
+            title: 'unseen',
+            icon: BsFillBookmarkDashFill,
+            sub_line: true
+        },
+        {
+            title: 'channels',
+            icon: BsFolderFill
+        },
+        {
+            title: 'groups',
+            icon: FaUserGroup,
+            sub_line: true
+        },
+        {
+            title: 'calls',
+            icon: BsFillTelephoneFill,
+            sub_line: true
+        },
+        {
+            title: 'calls',
+            icon: RiSettingsFill,
+        },
+    ]
+
+    const activeClassFolder = (id: number) => {
+        setIdButton(id)
+    }
+
+    const existButtonForActivated = (id: number) => {
+        return idButton === id
+    }
+
+    return (
+        <div className="py-14 grid place-items-center">
+            {folders.map((folder, index) => {
+                const checkActiveButton = existButtonForActivated(index)
+
+                return (
+                    <>
+                        <button
+                            className={`relative text-liver block my-2 py-2 ${checkActiveButton ? 'text-blue-picton-rgb' : ''}`}
+                            key={index} title={folder.title}
+                            onClick={() => activeClassFolder(index)}>
+                            <folder.icon size={22} className=""/>
+                            {checkActiveButton &&
+                                <p className="bg-blue-picton-rgb h-10 w-1 rounded-full absolute left-12 top-1 after:content-['']"/>}
+                        </button>
+
+                        {folder.sub_line && <p className="bg-liver h-px w-12 my-4 rounded-full after:content-['']"/>}
+                    </>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Folders
--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -1,10 +1,13 @@
 import ProfileStory from "@/app/components/sidebar/profile-story";
+import Folders from "@/app/components/sidebar/folders";
 
 const Sidebar = () => {
     return (
         <div className="py-4 px-3">
             {/* profile story */}
             <ProfileStory/>
+            {/* folders */}
+            <Folders/>
         </div>
     )
 }
