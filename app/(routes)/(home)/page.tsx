86eb9bad613c47f12c18c99fd31c9e0508d4f77a
mhmddev <mabrahimibagha@gmail.com>
2023-07-08 13:30:14 +0330

    feat: add stories

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,10 +1,12 @@
 import Sidebar from "@/app/components/sidebar";
 import Layout from "@/app/customLayout";
+import Chats from "@/app/components/chats";
 
 const Home = () => {
     return (
         <Layout>
             <Sidebar/>
+            <Chats/>
         </Layout>
     )
 }
---
+++ app/components/chats/index.tsx
@@ -0,0 +1,13 @@
+import Stories from "@/app/components/chats/stories";
+
+const Chats = () => {
+    return (
+        <>
+            <div className="bg-dark-jungle py-4 px-3 w-1/6 h-screen">
+                <Stories/>
+            </div>
+        </>
+    )
+}
+
+export default Chats
