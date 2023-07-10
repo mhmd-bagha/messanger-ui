b5421acdb68fc6a275753c7924a8a031e69f0238
mhmddev <mabrahimibagha@gmail.com>
2023-07-10 21:41:21 +0330

    feat: add chats

--- app/components/chats/index.tsx
+++ app/components/chats/index.tsx
@@ -1,5 +1,6 @@
 import Stories from "@/app/components/chats/stories";
 import Search from "@/app/components/chats/search";
+import Chat from "@/app/components/chats/chats";
 
 const Chats = () => {
     return (
@@ -7,6 +8,7 @@ const Chats = () => {
             <div className="bg-dark-jungle py-4 px-6 w-1/6 h-screen">
                 <Stories/>
                 <Search/>
+                <Chat/>
             </div>
         </>
     )
---
+++ app/types/chats/index.ts
@@ -0,0 +1,7 @@
+export type Chat = {
+    user_image: string,
+    user_name: string,
+    type_message: string,
+    status_message: boolean,
+    latest_time: string
+}
